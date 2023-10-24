da76148ed4d7659d5dff36c0e735cd666e078f8f
Edward Espinoza <em.esc.et@gmail.com>
2023-10-24 11:03:38 -0500

    Se modificó el límite a 20 segundos

--- src/controllers/validateCmp.js
+++ src/controllers/validateCmp.js
@@ -117,7 +117,7 @@ function fetchWithTimeout(url, validateOptions) {
             console.log(JSON.parse(validateOptions.body).numero);
             console.log('Tiempo de espera Agotado');
             resolve({});
-        }, 15000);
+        }, 20000);
     });
 
     return Promise.race([fetchPromise, timeoutPromise]);
